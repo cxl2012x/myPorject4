bd0e7f4d4ba519f73e1888bba157cfe347b7b051
chixl <chixl@jingyuk.com>
2019-10-21 18:16:02 +0800

    我的IDEA第1次提交

---
+++ gitTestProject/src/com/jingyuk/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.jingyuk.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("我是你的事实!");
+        System.out.println("我是你的事实!");
+        System.out.println("我是你的事实!");
+        System.out.println("我是你的事实!");
+        System.out.println("我是你的事实!");
+    }
+
+}

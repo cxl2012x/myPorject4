67e0b253daa92a0a656d15d38c85e9c579583855
chixl <chixl@jingyuk.com>
2019-10-21 18:18:52 +0800

    我的IDEA第2次提交

--- gitTestProject/src/com/jingyuk/git/FirstClass.java
+++ gitTestProject/src/com/jingyuk/git/FirstClass.java
@@ -7,6 +7,13 @@ public class FirstClass {
         System.out.println("我是你的事实!");
         System.out.println("我是你的事实!");
         System.out.println("我是你的事实!");
+        test();
     }
 
+    public static void test(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = "+i);
+            i +=1;
+        }
+    }
 }
